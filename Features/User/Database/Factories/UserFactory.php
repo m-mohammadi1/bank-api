36e4213911cef97f7aa782a3c1df4eecd202f694
Mr. Programmer lin <m.mohammadi.developer@gmail.com>
2022-06-29 12:21:13 +0430

    user section database completed

---
+++ Features/User/Database/Factories/UserFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class UserFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'phone_number' => fake()->phoneNumber,
+            'email' => fake()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ];
+    }
+}
