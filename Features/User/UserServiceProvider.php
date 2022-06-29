050c9aab9d00317a0fa49aa1a62a0e0043c11ca7
Mr. Programmer lin <m.mohammadi.developer@gmail.com>
2022-06-29 11:22:17 +0430

    user section strutured

---
+++ Features/User/UserServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Features\User;
+
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/config.php', 'user');
+    }
+
+
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'Database/Migrations/');
+
+        Route::prefix('api/v1/')
+            ->middleware('api')
+            ->group(__DIR__ . '/Routes/Api/api.v1.php');
+    }
+}
