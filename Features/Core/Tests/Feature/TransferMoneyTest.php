b6c463b8d551722408317c4af8a05f3fea2d1a4f
Mr. Programmer lin <m.mohammadi.developer@gmail.com>
2022-06-30 16:38:56 +0430

    add tests for transfer money

--- Features/Core/Tests/Feature/TransferMoneyTest.php
+++ Features/Core/Tests/Feature/TransferMoneyTest.php
@@ -2,17 +2,18 @@
 
 namespace Features\Core\Tests\Feature;
 
+use Features\Core\Events\TransactionCompletedEvent;
 use Features\Core\Models\Account;
 use Features\Core\Models\CreditCart;
 use Features\User\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class TransferMoneyTest extends TestCase
 {
     use RefreshDatabase;
-
 
     public function test_user_can_transfer_money_with_correct_information()
     {
@@ -59,6 +60,76 @@ class TransferMoneyTest extends TestCase
             'amount' => config('core.transaction_wage'),
             'transaction_id' => $user1Transaction->id
         ]);
+    }
+
+    public function test_user_cannot_transfer_money_with_incorrect_sender_information()
+    {
+        $amount = 500_000;
+
+        $phone_number1 = '09145687353';
+        $cart_number1 = '6037997557708709';
+        $balance1 = 1_000_000;
+
+        [$user1, $user1Account, $user1Cart] = $this->createUserWithAccountAndCreditCart($phone_number1, $cart_number1, $balance1);
+
+
+        $this
+            ->actingAs($user1)
+            ->postJson(route('v1.transfer_money'), [
+                'sender_cart_number' => '6037997557708712',
+                'recipient_cart_number' => '6104337613803152',
+                'amount' => $amount
+            ])
+            ->assertJsonValidationErrorFor('sender_cart_number');
+    }
+
+    public function test_user_cannot_transfer_money_with_incorrect_recevier_information()
+    {
+        $amount = 500_000;
+
+        $phone_number1 = '09145687353';
+        $cart_number1 = '6037997557708709';
+        $balance1 = 1_000_000;
+
+        [$user1, $user1Account, $user1Cart] = $this->createUserWithAccountAndCreditCart($phone_number1, $cart_number1, $balance1);
+
+
+        $this
+            ->actingAs($user1)
+            ->postJson(route('v1.transfer_money'), [
+                'sender_cart_number' => '6037997557708709',
+                'recipient_cart_number' => '61043376138031000',
+                'amount' => $amount
+            ])
+            ->assertJsonValidationErrorFor('recipient_cart_number');
+    }
+
+    public function test_user_cannot_transfer_money_with_incorrect_amount_information()
+    {
+        $phone_number1 = '09145687353';
+        $cart_number1 = '6037997557708709';
+        $balance1 = 1_000_000;
+
+        [$user1, $user1Account, $user1Cart] = $this->createUserWithAccountAndCreditCart($phone_number1, $cart_number1, $balance1);
+
+
+        $this
+            ->actingAs($user1)
+            ->postJson(route('v1.transfer_money'), [
+                'sender_cart_number' => '6037997557708709',
+                'recipient_cart_number' => '6104337613803152',
+                'amount' => 50
+            ])
+            ->assertJsonValidationErrorFor('amount');
+
+        $this
+            ->actingAs($user1)
+            ->postJson(route('v1.transfer_money'), [
+                'sender_cart_number' => '6037997557708709',
+                'recipient_cart_number' => '6104337613803152',
+                'amount' => 50_000_001
+            ])
+            ->assertJsonValidationErrorFor('amount');
     }
 
 
